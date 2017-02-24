dc92f12f69ed7ea64813907b89dde45b18fca36c
cave3 <wrks@mac.com>
2017-02-24 17:29:55 +0100

    Server setup and simple middleware logger function

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('CustomerApp')
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+// simple middleware function
+var logger = function(req, res, next) {
+  console.log('logging...');
+}
+app.use(logger);
+
+app.get('/', function(req, res) {
+  res.send('Hello World');
+});
+
+app.listen(3000, function() {
+  console.log('Server started on Port 3000...');
+});
