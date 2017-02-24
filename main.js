65f8c094b596d5d9b7ac1310e126551af655baf7
cave3 <wrks@mac.com>
2017-02-24 17:56:48 +0100

    renamed app.js, pass an array to res.json()

--- main.js
+++ main.js
@@ -19,8 +19,24 @@ app.use(bodyParser.urlencoded({extended: false}));
 // set static path
 app.use(express.static(path.join(__dirname, 'app')));
 
+var people = [
+  {
+    name: 'Sash',
+    age: 44
+  },
+  {
+    name: 'Misha',
+    age: 7
+  },
+  {
+    name: 'Lou',
+    age: 3
+  }
+]
+
 app.get('/', function(req, res) {
-  res.send('Hello World');
+  //res.send('Hello');
+  res.json(people);
 });
 
 app.listen(3000, function() {
