417e66537b34b4e70a0eda8521da1b2572b1c587
cave3 <wrks@mac.com>
2017-02-25 15:24:00 +0100

    Save and delete customers in the database

---
+++ app.js
@@ -0,0 +1,102 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var expressValidator = require('express-validator');
+var mongojs = require('mongojs');
+
+var ObjectId = mongojs.ObjectId;
+var db = mongojs('customerapp', ['users'])
+var app = express();
+
+// view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+// body-parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// set static path
+app.use(express.static(path.join(__dirname, 'app')));
+
+// Global Vars
+app.use(function(res, res, next){
+  res.locals.errors = null;
+  next();
+});
+
+// Express Validator Middleware (Form validation)
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+app.get('/', function(req, res) {
+  db.users.find(function (err, docs) {
+    res.render('index', {
+      title: 'Customers',
+      users: docs
+    });
+  })
+});
+
+// ADD user route
+app.post('/users/add', function(req, res){
+  // express-validator
+  req.checkBody('first_name', 'First Name is required').notEmpty();
+  req.checkBody('last_name', 'Last Name is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+
+  var errors = req.validationErrors();
+  if(errors) {
+    res.render('index', {
+      title: 'Customers',
+      users: users,
+      errors: errors
+    });
+  } else {
+    var newUser = {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email
+    }
+    console.log('SUCCESS');
+    db.users.insert(newUser, function(err, result) {
+      if(err) {
+        console.log(err);
+      }
+      res.redirect('/');
+    });
+  }
+});
+
+// REMOVE user route
+app.delete('/users/delete/:id', function(req, res){
+  //console.log(req.params.id);
+  db.users.remove({_id: ObjectId(req.params.id)}, function(err, result) {
+    if(err) {
+      console.log(err);
+    }
+    res.redirect('/');
+
+
+
+  });
+});
+
+app.listen(3000, function() {
+  console.log('Server started on Port 3000...');
+});
