d1f464eac11d201a3b15be7f2027ff94facd23d9
cave3 <wrks@mac.com>
2017-02-25 07:02:19 +0100

    added form, express-validator (An express.js middleware for node-validator)

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var expressValidator = require('express-validator');
 
 var app = express();
 
@@ -16,24 +17,49 @@ app.use(bodyParser.urlencoded({extended: false}));
 // set static path
 app.use(express.static(path.join(__dirname, 'app')));
 
+// Global Vars
+app.use(function(res, res, next){
+  res.locals.errors = null;
+  next();
+});
+
+// Express Validator Middleware (Form validation)
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// USERS
 var users = [
   {
     id: 1,
     first_name: 'John',
     last_name: 'Doe',
-    email:  'jdoe@gmail.com',
+    email:  'jdoe@gmail.com'
   },
   {
     id: 2,
     first_name: 'Bob',
     last_name: 'Smith',
-    email:  'bsmith@gmail.com',
+    email:  'bsmith@gmail.com'
   },
   {
     id: 3,
     first_name: 'Jill',
     last_name: 'Jackson',
-    email:  'jjackson@gmail.com',
+    email:  'jjackson@gmail.com'
   }
 ];
 
@@ -42,6 +68,29 @@ app.get('/', function(req, res) {
     title: 'Customers',
     users: users
   });
+});
+
+app.post('/users/add', function(req, res){
+  // express-validator
+  req.checkBody('first_name', 'First Name is required').notEmpty();
+  req.checkBody('last_name', 'Last Name is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+
+  var errors = req.validationErrors();
+  if(errors) {
+    res.render('index', {
+      title: 'Customers',
+      users: users,
+      errors: errors
+    });
+  } else {
+    var newUser = {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email
+    }
+    console.log('SUCCESS');
+  }
 });
 
 app.listen(3000, function() {
